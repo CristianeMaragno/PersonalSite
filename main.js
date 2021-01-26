68649afa0b7a0e9489ccf9b495f76988c515eda1
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2021-01-26 10:13:59 -0300

    Change slideshows to individual

--- main.js
+++ main.js
@@ -14,33 +14,83 @@ function open_resume(){
 
 }
 
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
 }
 
-function showSlides(n, no) {
+// Thumbnail image controls
+function currentSlide1(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
   var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var slides = document.getElementsByClassName("mySlides1");
+  if (n > slides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";   
+  slides[slideIndex1-1].style.display = "block";
 }
 
-function plusSlides2(n, no) {
-  showSlides2(slideIndex2[no] += n, no);
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
 }
 
-function showSlides2(n, no) {
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
   var i;
-  var x = document.getElementsByClassName(slideId2[no]);
-  if (n > x.length) {slideIndex2[no] = 1}    
-  if (n < 1) {slideIndex2[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  var slides = document.getElementsByClassName("mySlides2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  x[slideIndex2[no]-1].style.display = "block";  
+  slides[slideIndex2-1].style.display = "block";
 }
+
+
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}
+
+// Thumbnail image controls
+function currentSlide3(n) {
+  showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  if (n > slides.length) {slideIndex3 = 1}
+  if (n < 1) {slideIndex3 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex3-1].style.display = "block";
+}
+
+/*function plusSlides4(n) {
+  showSlides4(slideIndex4 += n);
+}
+
+// Thumbnail image controls
+function currentSlide4(n) {
+  showSlides4(slideIndex4 = n);
+}
+
+function showSlides4(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides4");
+  if (n > slides.length) {slideIndex4 = 1}
+  if (n < 1) {slideIndex4 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex4-1].style.display = "block";
+}*/
